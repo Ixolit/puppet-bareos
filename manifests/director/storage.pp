8bed48c1a770310544dbeb2e9c2a01efdc83b860
tmanninger <t.manninger@ixolit.com>
2024-06-07 10:08:11 +0200

    custom config

--- manifests/director/storage.pp
+++ manifests/director/storage.pp
@@ -306,7 +306,7 @@ define bareos::director::storage (
       [$maximum_concurrent_read_jobs, 'Maximum Concurrent Read Jobs', 'pint32', false],
       [$media_type, 'Media Type', 'strname', true],
       [$paired_storage, 'Paired Storage', 'res', false],
-      [$password, 'Password', 'autopassword', true],
+      [$password, 'Password', 'autopassword', false],
       [$port, 'Port', 'pint32', false],
       [$protocol, 'Protocol', 'auth_protocol_type', false],
       [$tape_device, 'Tape Device', 'string_list', false],
